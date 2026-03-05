f249f656cd8c82887778ba8bb04c01178d6dec7c
piyaphongdamrongtham46-tech <piyaphongdamrongtham46@gmail.com>
2026-03-05 15:39:11 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Student Web</title>
+</head>
+<body>
+
+<h1>Student Information</h1>
+
+<p>Name : Piyaphong damrongtham</p>
+<p>Student ID : 2651032341144</p>
+<p>Major : Information Technology</p>
+<p>Project : Docker Container Web Server</p>
+
+</body>
+</html>
